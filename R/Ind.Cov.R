16ebdc6fe1188a7de93b136534cf0f09bc48ff39
Yachen Yan <yanyachen21@gmail.com>
2015-03-01 00:00:00 +0000

    version 1.0.0

---
+++ R/Ind.Cov.R
@@ -0,0 +1,18 @@
+#' @title Independence opreator on Covariance Matrix
+#'
+#' @description
+#' Apply independence model on a covariance matrix.
+#'
+#' @param sigma a covariance matrix
+#' @return a regularized covariance matrix after applying independence model
+#' @examples
+#' data(m.excess.c10sp9003)
+#' cov.SAM <- cov(m.excess.c10sp9003)
+#' Ind.Cov(cov.SAM)
+#' @export
+
+Ind.Cov <- function(sigma){
+  COV <- diag(diag(cov(sigma)))
+  dimnames(COV) <- list(colnames(sigma),colnames(sigma))
+  return(COV)
+}
